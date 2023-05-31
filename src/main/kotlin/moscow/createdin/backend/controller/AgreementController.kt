dbf7567f6ebe589540f997697cc80f489b081338
Илюшечкин Александр Сергеевич <progruzovik@gmail.com>
2023-05-31 09:54:04 +0300

    add agreeement auth

--- src/main/kotlin/moscow/createdin/backend/controller/AgreementController.kt
+++ src/main/kotlin/moscow/createdin/backend/controller/AgreementController.kt
@@ -3,6 +3,7 @@ package moscow.createdin.backend.controller
 import moscow.createdin.backend.model.dto.AgreementDTO
 import moscow.createdin.backend.model.dto.CreateAgreementRequestDTO
 import moscow.createdin.backend.service.AgreementService
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/agreement")
 class AgreementController(private val agreementService: AgreementService) {
 
+    @PreAuthorize("hasRole('RENTER')")
     @PostMapping
     fun postAgreement(@RequestBody createAgreementRequest: CreateAgreementRequestDTO): AgreementDTO {
         return agreementService.createAgreement(createAgreementRequest)
